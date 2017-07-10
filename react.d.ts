649abc2aca8815fa4b2d6f18eec9e318a1a435c4
Tal Harel <talh@wix.com>
2017-07-10 17:32:21 +0300

    gh-pages setup

--- react.d.ts
+++
@@ -1,1 +0,0 @@
-export * from './src/stylable-component/stylable-react';
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { Resolver } from "./resolver";
-export { Stylesheet, TypedClass } from "./stylesheet";
-export { Generator, DEFAULT_CONFIG, Config } from "./generator";
